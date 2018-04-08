66bd977cbcd947bfd21b832b362fe38ee9229bec
Alex Wallish <alexwallish@wireless-10-104-228-143.umd.edu>
2018-04-07 22:07:27 -0400

    Initial Commit

---
+++ crypto/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  crypto
+//
+//  Created by Alex Wallish on 4/7/18.
+//  Copyright © 2018 Alex Wallish. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
