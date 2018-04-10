f003abefd7038eaa6b64d1e97c023c167312827f
Alex Wallish <37921054+awallish@users.noreply.github.com>
2018-04-10 18:32:46 -0400

    Update LocationIncentive.sol

--- contracts/LocationIncentive.sol
+++ contracts/LocationIncentive.sol
@@ -1,82 +1,51 @@
-pragma solidity 0.4.21;
-contract Casino {
-   address public owner;
-   uint256 public minimumBet;
-   uint256 public totalBet;
-   uint256 public numberOfBets;
-   uint256 public maxAmountOfBets = 100;
-   address[] public players;
-   struct Player {
-      uint256 amountBet;
-      uint256 numberSelected;
-   }
-   // The address of the player and => the user info   
-   mapping(address => Player) public playerInfo;
+pragma solidity ^0.4.21;
 
-   function Casino(uint256 _minimumBet) public {
-      owner = msg.sender;
-	  if (_minimumBet != 0) minimumBet = _minimumBet;
-   }
-   function kill() public {
-      if(msg.sender == owner) selfdestruct(owner);
-   }
-
-
-   function checkPlayerExists(address player) public constant returns(bool){
-      for(uint256 i = 0; i < players.length; i++){
-         if(players[i] == player) return true;
-      }
-      return false;
-   }
-
-   // To bet for a number between 1 and 10 both inclusive
-   function bet(uint256 numberSelected) public payable {
-      require(!checkPlayerExists(msg.sender));
-      require(numberSelected >= 1 && numberSelected <= 10);
-      require(msg.value >= minimumBet);
-      playerInfo[msg.sender].amountBet = msg.value;
-      playerInfo[msg.sender].numberSelected = numberSelected;
-      numberOfBets++;
-      players.push(msg.sender);
-      totalBet += msg.value;
-
-	  if(numberOfBets >= maxAmountOfBets) generateNumberWinner();
-   }
-
-      // Generates a number between 1 and 10 that will be the winner
-   function generateNumberWinner() public {
-      uint256 numberGenerated = block.number % 10 + 1; // This isn't secure
-      distributePrizes(numberGenerated);
-   }
-
-      // Sends the corresponding ether to each winner depending on the total bets
-   function distributePrizes(uint256 numberWinner) private {
-      address[100] memory winners; // We have to create a temporary in memory array with fixed size
-      uint256 count = 0; // This is the count for the array of winners
-      for(uint256 i = 0; i < players.length; i++){
-         address playerAddress = players[i];
-         if(playerInfo[playerAddress].numberSelected == numberWinner){
-            winners[count] = playerAddress;
-            count++;
-         }
-         delete playerInfo[playerAddress]; // Delete all the players
-      }
-      players.length = 0; // Delete all the players array
-      uint256 winnerEtherAmount = totalBet / winners.length; // How much each winner gets
-      for(uint256 j = 0; j < count; j++){
-         if(winners[j] != address(0)) // Check that the address in this fixed array is not empty
-         winners[j].transfer(winnerEtherAmount);
-      }
-      resetData();
-   }
-
-   // Simply for securing the funds
-   function() public payable {}
-   
-   function resetData() private{
-   players.length = 0; // Delete all the players array
-   totalBet = 0;
-   numberOfBets = 0;
-}
-
+contract LocationIncentive {  
+    
+    struct Incentive{
+        string name;
+        uint256 reward;
+        uint256 winningNumber;
+        uint256 currentVal;
+    }
+    
+    mapping(string => Incentive) incentives;
+    
+    //event log_number(uint256 log); // Event
+    event log_string(string log); //Event
+    //event log_address(address log); //Event
+    
+    function () public payable { 
+        // Fallback Function
+    }
+    
+    function createIncentive(string tL, string name, uint256 r, uint wN) public payable {
+        if(!(msg.value < r || r <= 0 || wN <= 0)) {
+            incentives[tL] = Incentive(name, r, wN, 0);
+            emit log_string("Success!");
+        } else {
+            emit log_string("Failure");
+        }
+    }
+    
+    function checkIn(string location) public {
+        if(_null(incentives[location])){
+            emit log_string("No incentive at this location!");
+        } else {
+            emit log_string(incentives[location].name);
+            incentives[location].currentVal++;
+            if(incentives[location].currentVal == incentives[location].winningNumber) {
+                emit log_string("WIN");
+                msg.sender.transfer(incentives[location].reward);
+                delete incentives[location];
+            }
+        }
+    }
+    
+    function _null(Incentive a) internal pure returns (bool) {
+        if(a.reward == 0 && a.winningNumber == 0 && a.currentVal == 0) {
+            return true;
+        }
+        return false;
+    }
 }
